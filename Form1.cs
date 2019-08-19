cbe7c64987919279e91da7ddb34d305552a665e0
Tomasz <plucioent@gmail.com>
2019-08-19 14:37:48 +0200

    seeing Db in texboxes

--- Form1.cs
+++ Form1.cs
@@ -18,38 +18,10 @@ namespace DemoDBApplication
             InitializeComponent();
         }
 
-        private void LoginBtn_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            string connectionString;
-            SqlConnection cnn;
-
-            connectionString = @"Data Source=ADMINRG-5NS3932\TEW_SQLEXPRESS;Initial Catalog=demoDB;User ID=sa;Password=demo123";
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-
-            SqlCommand sqlCommand;
-            SqlDataReader dataReader;
-            String sql, Output = string.Empty;
-
-            sql = "Select TutorialID,TutorialName from TutorialTable";
-
-            sqlCommand = new SqlCommand(sql, cnn);
-
-            dataReader = sqlCommand.ExecuteReader();
-            while (dataReader.Read())
-            {
-                Output = Output + dataReader.GetValue(0) + "-" + dataReader.GetValue(1) + "\n";
-            }
-
-            MessageBox.Show(Output);
-            dataReader.Close();
-            sqlCommand.Dispose();
-            cnn.Close();
-        }
-
-        private void InsertBtn_Click(object sender, EventArgs e)
-        {
+            // TODO: This line of code loads data into the 'demoDBDataSet.TutorialTable' table. You can move, or remove it, as needed.
+            this.tutorialTableTableAdapter.Fill(this.demoDBDataSet.TutorialTable);
 
         }
     }
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DemoDBApplication.Properties
-{
-
-
+namespace DemoDBApplication.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ADMINRG-5NS3932\\TEW_SQLEXPRESS;Initial Catalog=demoDB;User ID=sa;Pass" +
+            "word=demo123")]
+        public string demoDBConnectionString {
+            get {
+                return ((string)(this["demoDBConnectionString"]));
             }
         }
     }
